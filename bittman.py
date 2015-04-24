f5ddc42264925ad70c0a48623969f7e911f44ae0
Rahul <rksethuram9@gmail.com>
2015-04-23 17:18:57 -0700

    Initial file

---
+++ bittman.py
@@ -0,0 +1,2 @@
+q = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22%5ESPX%22%2C%22%5EVIX%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
+
